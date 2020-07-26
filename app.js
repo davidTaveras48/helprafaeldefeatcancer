89f8668a66140809d780fdb6613e2de4e9622ac1
David Taveras <davidtaveras48@gmail.com>
2020-07-25 20:31:03 -0400

    updating

---
+++ app.js
@@ -0,0 +1,30 @@
+// Your web app's Firebase configuration
+var config = {
+  apiKey: "AIzaSyCIKN1q-gAxxiecYSedO93DKWUBbuvPHZc",
+  authDomain: "helprafaeldefeatcancer.firebaseapp.com",
+  databaseURL: "https://helprafaeldefeatcancer.firebaseio.com",
+  projectId: "helprafaeldefeatcancer",
+  storageBucket: "helprafaeldefeatcancer.appspot.com",
+  messagingSenderId: "36499608611",
+  appId: "1:36499608611:web:43753b4a81a23398d98d96"
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+const dbRef = firebase.database().ref('dataSync');
+var countingViews;
+
+dbRef.once('value')
+.then(r => {
+  if(!countingViews) countingViews = r.val().visitas;
+})
+.then(()=>{
+  dbRef.set({
+    visitas: countingViews + 1
+  })
+})
+
+var userLang = navigator.language || navigator.userLanguage; 
+alert(userLang);
+
